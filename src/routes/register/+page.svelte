1bcc133286b841d91d92ac536a6b15664639a14e
andremfp <andremfp29@gmail.com>
2024-09-15 16:46:22 +0100

    refactor register page

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -2,12 +2,12 @@
 	import { register } from '../../lib/auth';
 	import { goto } from '$app/navigation';
 	import { darkMode } from '$lib/stores/darkModeStore';
+	import { showToast } from '$lib/utils';
 
 	let email = '';
 	let username = '';
 	let password = '';
 	let confirmPassword = '';
-	let errorMessage = '';
 
 	async function handleRegister() {
 		if (password !== confirmPassword) {
@@ -17,21 +17,12 @@
 
 		try {
 			await register(email, username, password);
-			goto('/'); // Redirect to home page on successful registration
+			goto('/');
 		} catch (error) {
 			console.error('Registration error:', error);
-			errorMessage = error instanceof Error ? error.message : 'Registration failed';
+			const errorMessage = error instanceof Error ? error.message : 'Registration failed';
 			showToast(errorMessage);
 		}
-	}
-
-	function showToast(message: string) {
-		const toast = document.createElement('div');
-		toast.textContent = message;
-		toast.className = `${$darkMode ? 'bg-fail-toast-bg-dark' : 'bg-fail-toast-bg-light'} text-fail-toast-text fixed top-4 left-1/2 transform -translate-x-1/2 py-2 px-6 rounded-md shadow-lg backdrop-blur-md`;
-		toast.style.whiteSpace = 'pre-line';
-		document.body.appendChild(toast);
-		setTimeout(() => toast.remove(), 5000);
 	}
 
 	function goBack() {
@@ -40,9 +31,7 @@
 </script>
 
 <div
-	class={$darkMode
-		? 'flex items-center justify-center bg-main-bg-dark text-text-dark'
-		: 'flex items-center justify-center bg-main-bg-light text-text'}
+	class="flex items-center justify-center bg-main-bg-light dark:bg-main-bg-dark text-text-light dark:text-text-dark"
 >
 	<div class="w-full max-w-md flex flex-col items-center">
 		<div class="w-full flex justify-start mb-6">
@@ -51,18 +40,8 @@
 			</button>
 		</div>
 
-		<div
-			class={$darkMode
-				? 'w-full bg-login-bg-dark shadow-ios rounded-lg p-6'
-				: 'w-full bg-login-bg-light shadow-ios rounded-lg p-6'}
-		>
-			<h1
-				class={$darkMode
-					? 'text-2xl font-bold mb-4 text-center text-text-dark'
-					: 'text-2xl font-bold mb-4 text-center text-text-light'}
-			>
-				Register
-			</h1>
+		<div class="w-full bg-login-bg-light dark:bg-login-bg-dark shadow-ios rounded-lg p-6">
+			<h1 class="text-2xl font-bold mb-4 text-center">Register</h1>
 			<form on:submit|preventDefault={handleRegister} class="space-y-4">
 				<div>
 					<label for="email" class="block font-semibold mb-1">Email:</label>
@@ -70,33 +49,27 @@
 						id="email"
 						type="email"
 						bind:value={email}
-						class={$darkMode
-							? 'w-full p-2 border rounded-md text-text-light focus:outline-none'
-							: 'w-full p-2 border rounded-md text-text-light focus:outline-none focus:ring-1'}
+						class="w-full p-2 border border-border-light dark:border-border-dark rounded-md text-text-light dark:text-input-text-dark bg-white dark:bg-input-bg-dark focus:outline-none focus:ring-1"
 						required
 					/>
 				</div>
 				<div>
-					<label class="block font-semibold mb-1" for="username">Username:</label>
+					<label for="username" class="block font-semibold mb-1">Username:</label>
 					<input
 						id="username"
 						type="text"
 						bind:value={username}
-						class={$darkMode
-							? 'w-full p-2 border rounded-md text-text-light focus:outline-none'
-							: 'w-full p-2 border rounded-md text-text-light focus:outline-none focus:ring-1'}
+						class="w-full p-2 border border-border-light dark:border-border-dark rounded-md text-text-light dark:text-input-text-dark bg-white dark:bg-input-bg-dark focus:outline-none focus:ring-1"
 						required
 					/>
 				</div>
 				<div>
-					<label class="block font-semibold mb-1" for="password">Password:</label>
+					<label for="password" class="block font-semibold mb-1">Password:</label>
 					<input
 						id="password"
 						type="password"
 						bind:value={password}
-						class={$darkMode
-							? 'w-full p-2 border rounded-md text-text-light focus:outline-none focus:ring-1'
-							: 'w-full p-2 border rounded-md text-text-light focus:outline-none focus:ring-1'}
+						class="w-full p-2 border border-border-light dark:border-border-dark rounded-md text-text-light dark:text-input-text-dark bg-white dark:bg-input-bg-dark focus:outline-none focus:ring-1"
 						required
 					/>
 				</div>
@@ -106,17 +79,13 @@
 						id="confirmPassword"
 						type="password"
 						bind:value={confirmPassword}
-						class={$darkMode
-							? 'w-full p-2 border rounded-md text-text-light focus:outline-none focus:ring-1'
-							: 'w-full p-2 border rounded-md text-text-light focus:outline-none focus:ring-1'}
+						class="w-full p-2 border border-border-light dark:border-border-dark rounded-md text-text-light dark:text-input-text-dark bg-white dark:bg-input-bg-dark focus:outline-none focus:ring-1"
 						required
 					/>
 				</div>
 				<button
 					type="submit"
-					class={$darkMode
-						? 'w-full py-2 bg-login-btn text-white rounded-md focus:outline-none focus:bg-login-btn-focus'
-						: 'w-full py-2 bg-login-btn text-white rounded-md focus:outline-none focus:bg-login-btn-focus'}
+					class="w-full py-2 bg-login-btn text-white rounded-md focus:outline-none focus:bg-login-btn-focus"
 				>
 					Register
 				</button>
