4a0599e15d18558a260ff161bfa8caa1cecaba0a
andremfp <andremfp29@gmail.com>
2024-10-13 11:38:39 +0100

    add .vercel to gitignore

--- .vercel/output/static/_app/immutable/chunks/darkModeStore.CYBHSUnS.js
+++
@@ -1,1 +0,0 @@
-import{w as t}from"./entry.Dqh1kYlX.js";const d=t(!1);if(typeof window<"u"){const e=localStorage.getItem("darkMode")==="true";d.set(e),e?document.documentElement.classList.add("dark"):document.documentElement.classList.remove("dark")}export{d};
