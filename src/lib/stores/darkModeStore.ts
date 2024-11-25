e06feb7200d0af291c6822a4843fc525d33cbd92
andremfp <andremfp29@gmail.com>
2024-11-25 10:00:09 +0000

    theme color logic

--- src/lib/stores/darkModeStore.ts
+++ src/lib/stores/darkModeStore.ts
@@ -1,3 +1,4 @@
+import { doc } from '@firebase/firestore';
 import { writable } from 'svelte/store';
 
 // Create a writable store for dark mode
@@ -11,7 +12,9 @@ if (typeof window !== 'undefined') {
 	// Apply the dark mode class immediately to avoid flicker
 	if (isDarkMode) {
 		document.documentElement.classList.add('dark');
+		document.querySelector('meta[name="theme-color"]')?.setAttribute('content', '#1F1F1F');
 	} else {
 		document.documentElement.classList.remove('dark');
+		document.querySelector('meta[name="theme-color"]')?.setAttribute('content', '#F0F0F0');
 	}
 }
