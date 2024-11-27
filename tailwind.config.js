4b82670f28d6999ccb5adce56d6fc3b1a5d0164b
andremfp <andremfp29@gmail.com>
2024-11-27 19:10:30 +0000

    footer height test

--- tailwind.config.js
+++ tailwind.config.js
@@ -51,7 +51,7 @@ export default {
 				'column-margin-top': '4rem',
 				divider: '1px',
 				'nav-height': '56px',
-				'footer-height': '56px'
+				'footer-height': '86px'
 			},
 			fontSize: {
 				'icon-xl': '1.75rem',
