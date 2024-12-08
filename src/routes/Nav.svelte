539ceda982e88a4d3516782f57fcfd97297cea0f
andremfp <andremfp29@gmail.com>
2024-12-08 18:00:03 +0000

    correct footer highlight

--- src/routes/Nav.svelte
+++ src/routes/Nav.svelte
@@ -34,7 +34,7 @@
 
 <nav
 	bind:this={nav}
-	class="fixed left-0 right-0 h-nav-height transition-all duration-0 z-10 flex items-center
+	class="fixed left-0 right-0 h-nav-height transition-all duration-200 z-10 flex items-center
     {scrollPosY > 145 && currentRoute !== '/lists'
 		? 'bg-nav-bg-scroll-light/95 dark:bg-nav-bg-scroll-dark/95 shadow-lg backdrop-blur-md'
 		: 'bg-main-bg-light dark:bg-main-bg-dark'}"
